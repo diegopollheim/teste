dd671988262ff6779cca724bfa692280d0757dab
Diego Pollheim <diegopollheim@gmail.com>
2022-06-23 20:54:22 -0300

    primeiro commit

---
+++ pages/page2.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+export default function Page2() {
+  return (
+    <div>
+      <h1>Pagina 2</h1>
+      <a href='/'>IR PARA PAGE 1</a>
+    </div>
+  );
+}
